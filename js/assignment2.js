87f77bca19afaa79b380126b600dcc1f6faa5658
jongsung-jay <jongsung.jay@gmail.com>
2021-02-12 00:09:48 -0500

    assign2

--- js/assignment2.js
+++ js/assignment2.js
@@ -1,28 +1,33 @@
-// const rowCnt = 15;
-// const columnCnt = 15;
-
-// document.write('<table border="1">');
-// for (let i = 1; i <= rowCnt; i++){
-//     document.write('<tr>');
-//     for(let j = 1; j <= columnCnt; j++){
-//         document.write('<td>');
-//         document.write(i + ',' + j);
-//         document.write('</td>');
+// function drawingTd(){
+//     const rowCnt = document.getElementById(btnTd1).nodeValue;
+//     const columnCnt = document.getElementById(btnTd2).nodeValue;
+    
+//     document.write('<table border="1">');
+//     for (let i = 1; i <= rowCnt; i++){
+//         document.write('<tr>');
+//         for(let j = 1; j <= columnCnt; j++){
+//             document.write('<td>');
+//             document.write(i + ',' + j);
+//             document.write('</td>');
 //     }
 //     document.write('</tr>');
 // }
 // document.write('</table>');
+// }
+// document.getElementById("calculateBtn").addEventListener("click",drawingTd,false);
 
-
-function fnCalCount(type, ths){
-    var $input = $(ths).parents("td").find("input[name='pop_out']");
-    var tCount = Number($input.val());
-    var tEqCount = Number($(ths).parents("tr").find("td.bseq_ea").html());
-    
-    if(type=='p'){
-        if(tCount < tEqCount) $input.val(Number(tCount)+1);
-        
-    }else{
-        if(tCount >0) $input.val(Number(tCount)-1);    
+function main(a1,a2){
+    let tag = "<table border='1'>";
+    let b1 = parseInt(a1);
+    let b2 = parseInt(a2);
+    for(i = 1; i <= b1; i++){
+        tag += "<tr>";
+        for(j = 1; j <= b2; j++){
+            tag += "<td>" + i + "," + j + "</td>";
         }
+        tag += "</tr>";
+    }
+    tag += "</table>";
+    resultTable.innerHTML = tag;
 }
+
